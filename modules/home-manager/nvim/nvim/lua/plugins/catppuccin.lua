b2c8b40781d5160adab70d224b7aa1f79748d3dd
raphaelladinig <raphaelladinig07@gmail.com>
2024-06-26 19:53:25 +0200

    update nvim & kitty theme sync

--- modules/home-manager/nvim/nvim/lua/plugins/catppuccin.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/catppuccin.lua
@@ -1,66 +1,27 @@
 return {
-	"catppuccin/nvim",
-	name = "catppuccin",
-	priority = 1000,
-	config = function()
-		if vim.env.TERM == "xterm-kitty" then
-			vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-				callback = function()
-					local flavour = vim.g.colors_name
+  "catppuccin/nvim",
+  name = "catppuccin",
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme("catppuccin-mocha");
 
-					if flavour:match("^catppuccin") then
-						local socket_files = io.popen("ls /tmp/kitty-*")
+    if vim.env.TERM == "xterm-kitty" then
+      vim.api.nvim_create_autocmd({ "ColorScheme" }, {
+        callback = function()
+          local flavour = vim.g.colors_name
 
-						if socket_files == nil then
-							print("No kitty sockets found")
-							return
-						end
+          if flavour:match("^catppuccin") then
+            local cmd = "kitten @ --to %s"
+              .. " set-colors -c "
+              .. vim.env.HOME
+              .. "/.config/kitty/themes/"
+              .. flavour
+              .. ".conf"
 
-						local cmd = "kitten @ --to unix:%s"
-							.. " set-colors -a -c "
-							.. vim.env.HOME
-							.. "/.config/kitty/themes/"
-							.. vim.g.colors_name
-							.. ".conf"
-
-						for socket_file in socket_files:lines() do
-							vim.fn.system(cmd:format(socket_file))
-						end
-
-						socket_files:close()
-					end
-				end,
-			})
-		end
-
-		local flavour_file = vim.fn.expand("$HOME") .. "/.flavour"
-
-		vim.api.nvim_create_autocmd({ "ColorScheme" }, {
-			callback = function()
-				local flavour = vim.g.colors_name
-
-				if flavour:match("^catppuccin") then
-					local f, err = io.open(flavour_file, "w")
-					if f then
-						f:write(flavour)
-						f:close()
-					else
-						print("Failed to open file: " .. err)
-					end
-				end
-			end,
-		})
-
-		if vim.fn.filereadable(flavour_file) == 1 then
-			local flavour = vim.fn.system("cat " .. flavour_file)
-
-			if flavour ~= "" then
-				vim.cmd("colorscheme " .. flavour)
-			else
-				vim.cmd("colorscheme catppuccin-mocha")
-			end
-		else
-			vim.cmd("colorscheme default")
-		end
-	end,
+            vim.fn.system(cmd:format(vim.fn.expand("$KITTY_LISTEN_ON")))
+          end
+        end,
+      })
+    end
+  end,
 }

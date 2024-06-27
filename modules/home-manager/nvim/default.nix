66ec8d7ce478f14e15dc57552eb6d7a689bac307
raphaelladinig <raphaelladinig07@gmail.com>
2024-06-27 14:06:32 +0200

    af

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -2,10 +2,6 @@
 
 {
   programs.neovim = 
-  let
-    toLua = str: "lua << EOF\n${str}\nEOF\n";
-    toLuaFile = file: "lua << EOF\n${builtins.readFile file}\nEOF\n";
-  in
   {
     enable = true;
 
@@ -23,47 +19,27 @@
       nvim-web-devicons
       {
         plugin = telescope-nvim;
-        config = toLuaFile ./plugins/nvim-telescope.lua;
+        type = "lua";
+        config = ''${builtins.readFile ./plugins/telescope.lua}'';
       }
       {
         plugin = catppuccin-nvim;
-        config = toLuaFile ./plugins/catppuccin.lua;
+        type = "lua";
+        config = lib.fileContents ./plugins/catppuccin.lua;
       }
       {
         plugin = oil-nvim;
-        config = toLuaFile ./plugins/oil.lua;
+        type = "lua";
+        config = ''${builtins.readFile ./plugins/oil.lua}'';
       }
     ];
 
     extraPackages = with pkgs; [
-    vim-language-server
-    lua-language-server
-    stylua
-    clang-tools
-    vscode-extensions.vadimcn.vscode-lldb
-    nil
-    nixfmt-rfc-style
-    nodePackages.vscode-html-languageserver-bin
-    emmet-language-server
-    nodePackages.vscode-css-languageserver-bin
-    tailwindcss-language-server
-    nodePackages.typescript-language-server
-    prettierd
-    pyright
-    black
-    python312Packages.debugpy
-    marksman
-    vale
-    nodePackages.vscode-json-languageserver-bin
-    nodePackages.bash-language-server
-    beautysh
-    bashdb
-    lemminx
-    texlab
-    jdt-language-server
-    vscode-extensions.vscjava.vscode-java-test
-    vscode-extensions.vscjava.vscode-java-debug
-    
+      vim-language-server
+      lua-language-server
+      stylua
+      nil
+      nixfmt-rfc-style
     ];
   };
 }
--- modules/home-manager/nvim/plugins/oil.lua
+++ modules/home-manager/nvim/plugins/oil.lua
@@ -1,1 +1,3 @@
+require("oil").setup()
+
 vim.keymap.set("n", "<leader>o", "<CMD>Oil<CR>")

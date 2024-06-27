2dfe45675587ad9211715f9e1800aeed3e3ff303
raphaelladinig <raphaelladinig07@gmail.com>
2024-06-27 10:08:53 +0200

    mason

--- modules/home-manager/nvim/default.nix
+++ modules/home-manager/nvim/default.nix
@@ -6,11 +6,32 @@
   };
 
   home.packages = with pkgs; [
-    vim-language-server
-    lua-language-server
-    stylua
-    clang-tools
-    nil
-    nixfmt-rfc-style
+    # vim-language-server
+    # lua-language-server
+    # stylua
+    # clang-tools
+    # vscode-extensions.vadimcn.vscode-lldb
+    # nil
+    # nixfmt-rfc-style
+    # nodePackages.vscode-html-languageserver-bin
+    # emmet-language-server
+    # nodePackages.vscode-css-languageserver-bin
+    # tailwindcss-language-server
+    # nodePackages.typescript-language-server
+    # prettierd
+    # pyright
+    # black
+    # python312Packages.debugpy
+    # marksman
+    # vale
+    # nodePackages.vscode-json-languageserver-bin
+    # nodePackages.bash-language-server
+    # beautysh
+    # bashdb
+    # lemminx
+    # texlab
+    # jdt-language-server
+    # vscode-extensions.vscjava.vscode-java-test
+    # vscode-extensions.vscjava.vscode-java-debug
   ];
 }
---
+++ modules/home-manager/nvim/nvim/lua/plugins/mason.lua
@@ -0,0 +1,50 @@
+return {
+  "williamboman/mason.nvim",
+  dependencies = {
+    "WhoIsSethDaniel/mason-tool-installer.nvim",
+  },
+  config = function()
+    require("mason").setup({
+      ui = {
+        border = "rounded",
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
+
+    require("mason-tool-installer").setup({
+      ensure_installed = {
+        "lua-language-server",
+        "vim-language-server",
+        "stylua",
+        "clangd",
+        "clang-format",
+        "codelldb",
+        "html-lsp",
+        "css-lsp",
+        "tailwindcss-language-server",
+        "emmet-language-server",
+        "typescript-language-server",
+        "prettier",
+        "pyright",
+        "debugpy",
+        "black",
+        "jdtls",
+        "java-debug-adapter",
+        "java-test",
+        "dart-debug-adapter",
+        "marksman",
+        "vale",
+        "texlab",
+        "bash-language-server",
+        "bash-debug-adapter",
+        "beautysh",
+        "json-lsp",
+        "lemminx",
+      },
+    })
+  end,
+}

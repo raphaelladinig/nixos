4e465ecca233b9cf5e344c60b1d6018ba0bad0ed
raphaelladinig <raphaelladinig07@gmail.com>
2024-06-30 15:30:46 +0200

    ts

--- modules/home-manager/ags/ags/main.ts
+++ modules/home-manager/ags/ags/main.ts
@@ -1,26 +1,8 @@
-const time = Variable('', {
-    poll: [1000, function() {
-        return Date().toString();
-    }],
-});
-
-const Bar = (monitor: number) => Widget.Window({
-    monitor,
-    name: `bar${monitor}`,
-    anchor: ['top', 'left', 'right'],
-    exclusivity: 'exclusive',
-    child: Widget.CenterBox({
-        start_widget: Widget.Label({
-            hpack: 'center',
-            label: 'Welcome to AGS!',
-        }),
-        end_widget: Widget.Label({
-            hpack: 'center',
-            label: time.bind(),
-        }),
-    }),
-});
+import { Bar } from "./widget/bar/bar";
 
 App.config({
-    windows: [Bar(0)],
+    style: "./style/bar.scss",
+    windows: [
+        Bar(),
+    ],
 });

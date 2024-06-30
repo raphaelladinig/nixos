38e4e888f8a47ef59ad02e1e8706353f804c1e97
raphaelladinig <raphaelladinig07@gmail.com>
2024-06-30 13:44:35 +0200

    lazygit

--- modules/home-manager/lazygit/default.nix
+++ modules/home-manager/lazygit/default.nix
@@ -1,1 +1,14 @@
-{ }
+{ pkgs, config, ... }:
+
+let
+  inherit (import ../../../variables.nix) flake;
+in
+{
+  home.file = {
+    ".config/lazygit".source = config.lib.file.mkOutOfStoreSymlink "${flake}/modules/home-manager/lazygit/lazygit";
+  };
+
+  home.packages = with pkgs; [
+    lazygit
+  ];
+}

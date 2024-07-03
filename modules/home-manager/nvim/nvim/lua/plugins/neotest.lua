fa57dc05270ae3ecb9c2edd28eb10b6db8408cd3
raphaelladinig <raphaelladinig07@gmail.com>
2024-07-03 09:56:21 +0200

    fix neotest

--- modules/home-manager/nvim/nvim/lua/plugins/neotest.lua
+++ modules/home-manager/nvim/nvim/lua/plugins/neotest.lua
@@ -1,25 +1,20 @@
 return {
-	"nvim-neotest/neotest",
-	dependencies = {
-		"nvim-neotest/nvim-nio",
-		"nvim-lua/plenary.nvim",
-		"nvim-treesitter/nvim-treesitter",
-		"rcasia/neotest-java",
-	},
-	config = function()
-		local neotest = require("neotest")
-		neotest.setup({
-			adapters = {
-				require("neotest-java"),
-			},
-		})
+  "nvim-neotest/neotest",
+  dependencies = {
+    "nvim-neotest/nvim-nio",
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    local neotest = require("neotest")
+    neotest.setup({})
 
-		vim.keymap.set("n", "<leader>tc", function()
-			neotest.run.run()
-		end)
+    vim.keymap.set("n", "<leader>tc", function()
+      neotest.run.run()
+    end)
 
-		vim.keymap.set("n", "<leader>tf", function()
-			neotest.run.run(vim.fn.expand("%"))
-		end)
-	end,
+    vim.keymap.set("n", "<leader>tf", function()
+      neotest.run.run(vim.fn.expand("%"))
+    end)
+  end,
 }

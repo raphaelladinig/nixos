408e7ec0dfe7d88cb72b90ed2d71d637d8aa6048
raphaelladinig <raphaelladinig07@gmail.com>
2024-07-03 09:54:39 +0200

    autoreload

--- modules/home-manager/ags/ags/main.ts
+++ modules/home-manager/ags/ags/main.ts
@@ -3,9 +3,18 @@ import Bar from "./widgets/Bar/Bar";
 
 const scss = `${App.configDir}/styles/main.scss`;
 const css = `/tmp/ags.css`;
-Utils.exec(`sassc ${scss} ${css}`);
-App.applyCss(css);
+
+Utils.monitorFile(
+  `${App.configDir}/styles`,
+
+  function () {
+    Utils.exec(`sassc ${scss} ${css}`);
+    App.resetCss();
+    App.applyCss(css);
+  },
+);
 
 App.config({
+  style: css,
   windows: [...forMonitors(Bar)],
 });

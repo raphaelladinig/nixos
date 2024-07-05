f15fe43e345286fdc34eca2744af91bae5629463
raphaelladinig <raphaelladinig07@gmail.com>
2024-07-05 12:17:12 +0200

    update bar

--- modules/home-manager/ags/ags/widget/bar/network.ts
+++ modules/home-manager/ags/ags/widget/bar/network.ts
@@ -1,11 +1,17 @@
 const network = await Service.import("network");
 
 export default () => {
-  return Widget.Icon({
+  return Widget.Box({
     class_name: "network",
-  }).hook(network, (self) => {
-    const icon = network[network.primary || "wifi"]?.icon_name;
-    self.icon = icon || "";
-    self.visible = !!icon;
+    children: [
+      Widget.Icon({}).hook(network, (self) => {
+        const icon = network[network.primary || "wifi"]?.icon_name;
+        self.icon = icon || "";
+        self.visible = !!icon;
+      }),
+      Widget.Label().hook(network, (self) => {
+        self.label = network.wifi.ssid || "";
+      }),
+    ],
   });
 };

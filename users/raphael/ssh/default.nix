599c8d2d92a189bfa5fb95f948e9e32c8a17b2cc
raphaelladinig <raphaelladinig07@gmail.com>
2024-07-14 16:21:36 +0200

    fix ssh

--- users/raphael/ssh/default.nix
+++ users/raphael/ssh/default.nix
@@ -7,13 +7,13 @@ in
   imports = [ ../sops.nix ];
 
   home.file = {
-    ".ssh/ssh.pub".source = config.lib.file.mkOutOfStoreSymlink "${flake}/users/raphael/ssh/ssh.pub";
+    ".ssh/id_ed25519.pub".source = config.lib.file.mkOutOfStoreSymlink "${flake}/users/raphael/ssh/id_ed25519";
   };
 
   sops = {
     secrets = {
       ssh-key = {
-        path = "/home/raphael/.ssh/ssh";
+        path = "/home/raphael/.ssh/id_ed25519";
       };
     };
   };

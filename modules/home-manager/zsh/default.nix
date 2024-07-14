d941b69995b7870a490b2325b865b7763a676960
raphaelladinig <raphaelladinig07@gmail.com>
2024-07-14 09:09:38 +0200

    p10k

--- modules/home-manager/zsh/default.nix
+++ modules/home-manager/zsh/default.nix
@@ -6,6 +6,7 @@ in
 {
   home.file = {
     ".zshrc".source = config.lib.file.mkOutOfStoreSymlink "${flake}/modules/home-manager/zsh/.zshrc";
+    ".p10k.zsh".source = config.lib.file.mkOutOfStoreSymlink "${flake}/modules/home-manager/zsh/.p10k.zsh";
   };
   
   home.packages = with pkgs; [

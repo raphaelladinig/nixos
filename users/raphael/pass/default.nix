78607d8fc5d6aed91b825030c0e021186de69186
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-07-16 18:01:28 +0200

    idk

--- users/raphael/pass/default.nix
+++ users/raphael/pass/default.nix
@@ -4,9 +4,9 @@ let
   inherit (import ../../../lib/config.nix) flake;
 in
 {
-  # home.file = {
-  #   ".password-store/".source = config.lib.file.mkOutOfStoreSymlink "${flake}/users/raphael/pass/.password-store/";
-  # };
+  home.file = {
+    ".password-store/".source = config.lib.file.mkOutOfStoreSymlink "${flake}/users/raphael/pass/.password-store/";
+  };
 
   home.packages = with pkgs; [ pass ];
 }

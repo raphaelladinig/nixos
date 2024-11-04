7f8d1512d3962631b863a016e0c44f2f94e7cdbf
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-11-04 12:41:14 +0100

    mariadb

--- hosts/common/school.nix
+++ hosts/common/school.nix
@@ -3,6 +3,11 @@
 {
   services.udev.packages = with pkgs; [ platformio-core ];
 
+  services.mysql = {
+    enable = true;
+    package = pkgs.mariadb;
+  };
+
   environment.systemPackages = with pkgs; [
     platformio-core
   ];

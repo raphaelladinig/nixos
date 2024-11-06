99159d38a8f4b67f4cf4840c6db242ef1627e81c
Raphael Ladinig <raphaelladinig07@gmail.com>
2024-11-06 08:19:01 +0100

    lazyload avante

--- home/raphael/neovim/plugins/avante/config.lua
+++ home/raphael/neovim/plugins/avante/config.lua
@@ -1,8 +1,11 @@
-require("avante_lib").load()
-
-require("avante").setup({
-  provider = "copilot",
-  hints = { enabled = false },
+vim.api.nvim_create_autocmd("VimEnter", {
+  callback = function()
+    require("avante_lib").load()
+    require("avante").setup({
+      provider = "copilot",
+      hints = { enabled = false },
+    })
+  end,
 })
 
 require("render-markdown").setup({
